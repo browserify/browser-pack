d0df289e4a873ad632977e31578e4e9feaf933bc
James Halliday <mail@substack.net>
2013-02-10 23:49:11 -0800

    working example input and output

---
+++ index.js
@@ -0,0 +1,54 @@
+var JSONStream = require('JSONStream');
+var duplexer = require('duplexer');
+var through = require('through');
+
+var prelude = [
+    '(function(p,c,e){',
+        'function r(n){',
+            'if(!c[n]){',
+                'c[n]={exports:{}};',
+                'p[n][0](function(x){',
+                    'return r(p[n][1][x])',
+                '},c[n],c[n].exports);',
+            '}',
+            'return c[n].exports',
+        '}',
+        'for(var i=0;i<e.length;i++)r(e[i]);',
+        'return r',
+    '})({'
+].join('');
+
+module.exports = function () {
+    var parser = JSONStream.parse([ true ]);
+    var output = through(write, end);
+    parser.pipe(output);
+    
+    var first = true;
+    var entries = [];
+    
+    return duplexer(parser, output);
+    
+    function write (row) {
+        if (first) output.emit('data', prelude);
+        
+        this.emit('data', [
+            (first ? '' : ','),
+            JSON.stringify(row.id),
+            ':[',
+            'function(require,module,exports){' + row.source + '}',
+            ',',
+            JSON.stringify(row.deps || {}),
+            ']'
+        ].join(''));
+        
+        first = false;
+        entries.push(row.id);
+    }
+    
+    function end () {
+        if (first) output.emit('data', prelude);
+        
+        this.emit('data', '},{},' + JSON.stringify(entries) + ')');
+        this.emit('end');
+    }
+};

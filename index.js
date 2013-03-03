2d631596ad26a2bc93e6095967f29e7c1e7d9a33
Thorsten Lorenz <thlorenz@gmx.de>
2013-03-03 16:17:40 -0500

    adding sourceMappingURL support

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ var uglify = require('uglify-js');
 var fs = require('fs');
 var path = require('path');
 
+var createGenerator = require('inline-source-map');
+
 var prelude = (function () {
     var src = fs.readFileSync(path.join(__dirname, 'prelude.js'), 'utf8');
-    return uglify(src) + '(typeof require!=="undefined"&&require,{'
+    return uglify(src) + '(typeof require!=="undefined"&&require,{';
 })();
+
+function newlinesIn(src) {
+  if (!src) return 0;
+  var newlines = src.match(/\n/g);
+
+  return newlines ? newlines.length : 0;
+}
 
 module.exports = function (opts) {
     if (!opts) opts = {};
@@ -21,20 +30,38 @@ module.exports = function (opts) {
     var entries = [];
     var order = []; 
     
+    var lineno = 1 + newlinesIn(prelude);
+    var generator;
+
     return duplexer(parser, output);
     
+    function addMappings(row) {
+      generator = generator || createGenerator();
+      var offset = { line: lineno, column: 0 };
+
+      if (row.mappings && row.mappings.length)
+          generator.addMappings(row.sourceFile, row.mappings, offset);
+      else 
+          generator.addGeneratedMappings(row.sourceFile, row.source, offset);
+    }
+
     function write (row) {
         if (first) this.queue(prelude);
         
-        this.queue([
+        if (row.sourceFile) addMappings(row);
+
+        wrappedSource = [
             (first ? '' : ','),
             JSON.stringify(row.id),
             ':[',
-            'function(require,module,exports){' + row.source + '\n}',
+            'function(require,module,exports){\n' + row.source + '\n}',
             ',',
             JSON.stringify(row.deps || {}),
             ']'
-        ].join(''));
+        ].join('');
+
+        this.queue(wrappedSource);
+        lineno += newlinesIn(wrappedSource);
         
         first = false;
         if (row.entry && row.order !== undefined) {
@@ -47,6 +74,8 @@ module.exports = function (opts) {
         if (first) this.queue(prelude);
         
         this.queue('},{},' + JSON.stringify(entries) + ')');
+        if (generator) this.queue('\n' + generator.inlineMappingUrl());
+
         this.queue(null);
     }
 };

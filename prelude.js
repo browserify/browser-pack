764ad41739710f5292ef57e741c6ec786da6500e
James Halliday <mail@substack.net>
2013-03-24 12:46:16 -0700

    parent req no longer needed

--- prelude.js
+++ prelude.js
@@ -7,7 +7,7 @@
 // anything defined in a previous bundle is accessed via the
 // orig method which is the requireuire for previous bundles
 
-(function(parent_req, modules, cache, entry) {
+(function(modules, cache, entry) {
     function inner_req(name, jumped){
         if(!cache[name]) {
             if(!modules[name]) {
@@ -15,7 +15,6 @@
                 // current root require go all requires down from there
                 var rootRequire = typeof require == "function" && require;
                 if (!jumped && rootRequire) return rootRequire(name, true);
-                if (parent_req) return parent_req(name);
                 throw new Error('Cannot find module \'' + name + '\'');
             }
             var m = cache[name] = {exports:{}};

c556ea1e42409dcda167318ade6c5e84a2be116c
Schabse Laks <Dev@SLaks.net>
2013-05-08 19:32:48 -0400

    Implement module.parent
    This retains compatible with older preludes (although parent will be
    null)

--- prelude.js
+++ prelude.js
@@ -11,26 +11,29 @@
     // Save the require from previous bundle to this closure if any
     var previousRequire = typeof require == "function" && require;
 
-    function newRequire(name, jumped){
+    function newRequire(name, jumped, parent){
         if(!cache[name]) {
             if(!modules[name]) {
                 // if we cannot find the the module within our internal map or
                 // cache jump to the current global require ie. the last bundle
                 // that was added to the page.
                 var currentRequire = typeof require == "function" && require;
-                if (!jumped && currentRequire) return currentRequire(name, true);
+                if (!jumped && currentRequire) return currentRequire(name, true, parent);
 
                 // If there are other bundles on this page the require from the
                 // previous one is saved to 'previousRequire'. Repeat this as
                 // many times as there are bundles until the module is found or
                 // we exhaust the require chain.
-                if (previousRequire) return previousRequire(name, true);
+                if (previousRequire) return previousRequire(name, true, parent);
                 throw new Error('Cannot find module \'' + name + '\'');
             }
-            var m = cache[name] = {exports:{}};
+            var m = cache[name] = {
+                exports: {},
+                parent: parent || null
+            };
             modules[name][0].call(m.exports, function(x){
                 var id = modules[name][1][x];
-                return newRequire(id ? id : x);
+                return newRequire(id ? id : x, false, m);
             },m,m.exports);
         }
         return cache[name].exports;

6c2d80f8b00d79ce3d0dcfdd8a980a1f2cb9e454
Renée Kooi <renee@kooi.me>
2018-09-30 16:51:30 +0200

    Fix prepublish on windows

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -6,8 +6,8 @@ var concat = require('concat-stream');
 var path = require('path');
 
 var uglify = spawn(
-    require.resolve('uglify-js/bin/uglifyjs'),
-    ['--wrap-iife', '-c', 'side_effects=false,screw_ie8=false', '--verbose', '--ie8', '--passes=5', '-m', '--source-map', '--', '-']
+    'node',
+    [require.resolve('uglify-js/bin/uglifyjs'), '--wrap-iife', '-c', 'side_effects=false,screw_ie8=false', '--verbose', '--ie8', '--passes=5', '-m', '--source-map', '--', '-']
 );
 
 fs.createReadStream(path.join(__dirname, '..', 'prelude.js'))
